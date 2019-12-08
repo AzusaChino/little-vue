ccb7825a22858d8e9eb4319adb5945e5a8ba8616
AzusaChino <azusa146@gmail.com>
2019-12-08 22:49:53 +0800

    update little_koa package.json

--- little_koa/server/app.js
+++ little_koa/server/app.js
@@ -3,7 +3,7 @@ const app = new Koa()
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
+const bodyParser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
 const index = require('./routes/index')
@@ -12,15 +12,15 @@ const users = require('./routes/users')
 // error handler
 onerror(app)
 
-// middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+// middleWares
+app.use(bodyParser({
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(require('koa-static')(`${__dirname}/public`))
 
-app.use(views(__dirname + '/views', {
+app.use(views(`${__dirname}/views`, {
   extension: 'pug'
 }))
 
@@ -39,6 +39,6 @@ app.use(users.routes(), users.allowedMethods())
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
-});
+})
 
 module.exports = app

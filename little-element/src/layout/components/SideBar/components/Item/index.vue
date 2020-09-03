da1c6c3049acfc565f241a8afc7297ab85b7df2e
AzusaChino <azusa146@gmail.com>
2020-09-04 00:52:10 +0800

    ft. add little-element side bar (need to be updated)

---
+++ little-element/src/layout/components/SideBar/components/Item/index.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'MenuItem',
+  functional: true,
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  render(createElement, context) {
+    // react 风格
+    const { icon, title } = context.props
+    const virtualNodes = []
+    if (icon) {
+      if (icon.includes('el-icon')) {
+        virtualNodes.push(<i class={[icon, 'sub-el-icon']}/>)
+      } else {
+        virtualNodes.push(<svg-icon icon-class={icon}/>)
+      }
+      if (title) {
+        virtualNodes.push(<span slot='title'> {(title)} </span>)
+      }
+    }
+    return virtualNodes
+  }
+}
+</script>
+
+<style scoped>
+.sub-el-icon {
+  color: currentColor;
+  width: 1em;
+  height: 1em;
+}
+</style>
